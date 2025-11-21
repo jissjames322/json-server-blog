c42a3dbdc33a1515024601c9c3a682bfec5e887d
jissjames322 <jissjames322@gmail.com>
2025-11-21 21:42:57 +0530

    Configured Backend PORTS

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
 const port = process.env.PORT || 3000;
 
+// Use default middlewares
 server.use(middlewares);
+
+// Use router
 server.use(router);
 
-server.listen(port);
+// Bind to 0.0.0.0
+server.listen(port, '0.0.0.0', () => {
+  console.log(`JSON Server is running at http://0.0.0.0:${port}`);
+});
